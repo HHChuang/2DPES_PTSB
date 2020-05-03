89923470750f5ec27d53b3ca3e6fd7886f5d2046
Grace <d02223134@g.ntu.edu.tw>
2020-05-03 23:26:53 +0800

    plot_time_traj.sh: Modify the description of x-axis from VRI region to TSS1 \to TSS2

--- src/plotAnalyticalPES.py
+++ src/plotAnalyticalPES.py
@@ -8,68 +8,84 @@
 #   2020/04/24, Grace, add sympy (calculate analytical gradient/Hessian)    #
 #   2020/04/25, Grace, formulate Hessian matrix and then diagonalize it.    #
 #   2020/04/27, Grace, formulate gradient descent path; (IRC/IRP).          #
+#   2020/04/30, Grace, add contour line and modify stationaryPts().         #
 #                                                                           #
 #############################################################################
-
+import sys
 import numpy as np
 import sympy as sp
+from sympy.solvers.solveset import solveset, solveset_real, nonlinsolve
 import math
 # from sympy.plotting.plot import plot_contour
 import matplotlib.pyplot as plt
 
 
 def sp_symPES(X, Y):
-    #
-    sp_symPES = 44730.4129 \
-        - 66786.5363 * sp.cos(Y) \
-        + 26352.6908 * sp.cos(2*Y) \
-        - 3117.3613 * sp.cos(4*Y) \
-        - 659.3217 * sp.cos(6*Y) \
-        + 621.9640 * sp.sin(3*X) * sp.sin(Y) \
-        - 138.3050 * sp.sin(3*X) * sp.sin(2*Y) \
-        - 111.5488 * sp.cos(8*Y) \
-        + 41.8227 * sp.sin(3*X) * sp.sin(4*Y) \
-        - 7.7979 * sp.sin(3*X) * sp.sin(6*Y) \
-        + 9.9258 * sp.cos(6*X)\
-        - 19.0681 * sp.cos(6*X) * sp.cos(Y) \
-        + 600 * sp.cos(6*X) * sp.cos(2*Y) \
-        - 500 * sp.sin(3*Y)
-    #
-    # sp_symPES = (1/2) * (X*Y**2 - Y*X**2 - 2*X + 2*Y) \
-    #     + (1/30) * (X**4 + Y**4)
-    #
+    # Theor Chem Acc 1998, 100, 285-299.
+    # https://f1000.com/work/item/3809905/resources/2861282/pdf
     # sp_symPES = 2*Y + Y**2 + X**2*(0.4*X**2+Y)
+
+    # Theor Chem Acc 2004, 112, 40-51.
+    # https://f1000.com/work/item/5637171/resources/4634218/pdf
+    sp_symPES = (1/2.0) * (X*Y**2 - Y*X**2 - 2.0*X + 2.0*Y) \
+        + (1/30.0) * (X**4 + Y**4)
+
+    #
+    # sp_symPES = 44730.4129 \
+    #     - 66786.5363 * sp.cos(Y) \
+    #     + 26352.6908 * sp.cos(2*Y) \
+    #     - 3117.3613 * sp.cos(4*Y) \
+    #     - 659.3217 * sp.cos(6*Y) \
+    #     + 621.9640 * sp.sin(3*X) * sp.sin(Y) \
+    #     - 138.3050 * sp.sin(3*X) * sp.sin(2*Y) \
+    #     - 111.5488 * sp.cos(8*Y) \
+    #     + 41.8227 * sp.sin(3*X) * sp.sin(4*Y) \
+    #     - 7.7979 * sp.sin(3*X) * sp.sin(6*Y) \
+    #     + 9.9258 * sp.cos(6*X)\
+    #     - 19.0681 * sp.cos(6*X) * sp.cos(Y) \
+    #     + 600 * sp.cos(6*X) * sp.cos(2*Y) \
+    #     - 500 * sp.sin(3*Y)
 
     return sp_symPES
 
 
 def np_symPES(X, Y):
-    #
-    np_symPES = 44730.4129 \
-        - 66786.5363 * np.cos(Y) \
-        + 26352.6908 * np.cos(2*Y) \
-        - 3117.3613 * np.cos(4*Y) \
-        - 659.3217 * np.cos(6*Y) \
-        + 621.9640 * np.sin(3*X) * np.sin(Y) \
-        - 138.3050 * np.sin(3*X) * np.sin(2*Y) \
-        - 111.5488 * np.cos(8*Y) \
-        + 41.8227 * np.sin(3*X) * np.sin(4*Y) \
-        - 7.7979 * np.sin(3*X) * np.sin(6*Y) \
-        + 9.9258 * np.cos(6*X)\
-        - 19.0681 * np.cos(6*X) * np.cos(Y) \
-        + 600 * np.cos(6*X) * np.cos(2*Y) \
-        - 500 * np.sin(3*Y)
-    #
-    # np_symPES = (1/2) * (X*Y**2 - Y*X**2 - 2*X + 2*Y) \
-    #     + (1/30) * (X**4 + Y**4)
 
     # np_symPES = 2*Y + Y**2 + X**2*(0.4*X**2+Y)
+
+    np_symPES = (1/2) * (X*Y**2 - Y*X**2 - 2*X + 2*Y) \
+        + (1/30) * (X**4 + Y**4)
+
+    #
+    # np_symPES = 44730.4129 \
+    # - 66786.5363 * np.cos(Y) \
+    # + 26352.6908 * np.cos(2*Y) \
+    # - 3117.3613 * np.cos(4*Y) \
+    # - 659.3217 * np.cos(6*Y) \
+    # + 621.9640 * np.sin(3*X) * np.sin(Y) \
+    # - 138.3050 * np.sin(3*X) * np.sin(2*Y) \
+    # - 111.5488 * np.cos(8*Y) \
+    # + 41.8227 * np.sin(3*X) * np.sin(4*Y) \
+    # - 7.7979 * np.sin(3*X) * np.sin(6*Y) \
+    # + 9.9258 * np.cos(6*X)\
+    # - 19.0681 * np.cos(6*X) * np.cos(Y) \
+    # + 600 * np.cos(6*X) * np.cos(2*Y) \
+    # - 500 * np.sin(3*Y)
+
     return np_symPES
+
+
+def sp_asymPES(X, Y):
+    # np_asymPES = np_symPES(X, Y) + 500*X*Y
+    np_asymPES = (1/2) * (X*Y**2 - Y*X**2 - 1.5*X + 2*Y) \
+        + (1/30) * (X**4 + Y**4)
+
+    return np_asymPES
 
 
 def np_asymPES(X, Y):
     # np_asymPES = np_symPES(X, Y) + 500*X*Y
-    np_asymPES = (1/2) * (X*Y**2 - Y*X**2 - 1.75*X + 2*Y) \
+    np_asymPES = (1/2) * (X*Y**2 - Y*X**2 - 1.5*X + 2*Y) \
         + (1/30) * (X**4 + Y**4)
 
     return np_asymPES
@@ -81,7 +97,50 @@ def stationaryPts(E, x, y):
     G = [sp.diff(E, x), sp.diff(E, y)]
 
     # Find stationary points; i.e. G = 0
-    roots = sp.solve([sp.Eq(G[0], 0), sp.Eq(G[1], 0)], [x, y])
+    # find a valid solver (solve, solveset, nonlinsolve)
+    # ref.
+    # 1. sympy library: https://docs.sympy.org/latest/modules/solvers/solveset.html
+
+    # roots = sp.solveset([G[0], G[1]], [x, y], domain=sp.S.Reals)
+    # roots = sp.solve([G[0], G[1]], [x, y], manual=True, set=True)
+    # force=True , manual=True, set=True)
+    # roots = nonlinsolve([sp.Eq(G[0], 0), sp.Eq(G[1], 0)], [x, y])
+
+    # 2. online solver: https://www.wolframalpha.com/input/?i=solve%7B%281%2F2%29%28y%5E2-2xy-2%29%2B%284%2F30%29%28x%5E3%29%3D%3D0%2C%281%2F2%29%282xy-x%5E2%2B2%29%2B%284%2F30%29%28y%5E3%29%3D%3D0%7D
+
+    # symPES
+    roots = [
+        [-0.849174, 0.849174],
+        [0.789279, -0.789279],
+        [-1.14427, -3.07012],
+        [3.07012, 1.14427]
+    ]
+    # asymPES
+    # mu = 1.75
+    # roots = [
+    #     [-0.879504, 0.765168],
+    #     [0.884098, -0.648009],
+    #     [-1.19982, -3.11036],
+    #     [2.98781, 1.09984]
+    # ]
+    # mu = 1.5
+    # roots = [
+    #     [-0.916175, 0.678958],
+    #     [-1.25225, -3.14745],
+    #     [0.987403, -0.501976],
+    #     [2.89612, 1.04955]
+    # ]
+    # mu = 1.0
+    # roots = [
+    #     [-1.0080658794313, 0.50500000616268894],
+    #     [-1.34948, -3.2140],
+    #     [1.24222, -0.183244],
+    #     [2.66755, 0.919982]
+    # ]
+
+    # print(roots)
+    # exit()
+
     nroots = len(roots)
     # for i in range(nroots):
     #     sp.pprint(roots[i])
@@ -100,16 +159,21 @@ def stationaryPts(E, x, y):
         Htest = Hessian.subs([(x, roots[i][0]), (y, roots[i][1])])
         # sp.pprint(Htest)
 
-        # only check the sign first eigenvalue for a 2D Hessian
-        # print(Htest.eigenvects()[0][0])  # ascending order
-        if Htest.eigenvects()[0][0] > 0:
+        # check the smallest eigenvalue for a 2D Hessian
+        if Htest.eigenvects()[0][0] > Htest.eigenvects()[1][0]:
+            index = 1
+        else:
+            index = 0
+
+        # print(Htest.eigenvects()[index][0])
+        if Htest.eigenvects()[index][0] > 0:
             rootcol[i] = 'bo'  # minimum
         else:
             rootcol[i] = 'ro'  # saddle point
 
-        # store the first eigenvector for each point
-        Hvec[i] = [Htest.eigenvects()[0][2][0][0],
-                   Htest.eigenvects()[0][2][0][1]]
+        # store the selected eigenvector for each point
+        Hvec[i] = [Htest.eigenvects()[index][2][0][0],
+                   Htest.eigenvects()[index][2][0][1]]
 
     return G, Hvec, roots, rootcol
 
@@ -152,25 +216,37 @@ def GD(firstPts, E, G, Hvec):
              ]
         )
         # print(Gnorm)
-        if Gnorm <= 10e-4:
+        if Gnorm <= 10e-2:
             break
 
     return path
 
 
 def plotPES(np_func):
-    #
-    x = np.linspace(0.0, 1.0, 100)
-    y = np.linspace(-1.2, -0.2, 100)
 
-    # x = np.linspace(-2.5, 2.5, 100)  # [-4,4]
+    # 1.
+    # x = np.linspace(-2.5, 2.5, 100)
     # y = np.linspace(-3.0, 0.5, 100)
+
+    # 2.
+    x = np.linspace(-2.5, 3.2, 100)
+    y = np.linspace(-3.3, 2.0, 100)
+
+    # 3.
+    # x = np.linspace(0.0, 1.0, 100)
+    # y = np.linspace(-1.2, -0.2, 100)
+
     X, Y = np.meshgrid(x, y)
     E = np_func(X, Y)
 
     # aspect ratio of x and y is equal
     plt.gca().set_aspect('equal')
-    plt.contour(X, Y, E, 15, colors='black', linestyles='-')
+    symlevel = [-2.0, -0.8, 0, 0.8, 1.2]
+    asymlevel = [-2.5, -1.5, -0.5, 0, 0.5, 1.0]
+    fig = plt.contour(X, Y, E, 100, colors='black',
+                      linestyles='-', levels=symlevel)
+    # label the contour lines
+    plt.clabel(fig, inline=True, fmt='%1.1f', fontsize=8)
 
 
 def plotPts(roots, rootcol):
@@ -199,14 +275,16 @@ if __name__ == "__main__":
 
     # Find stationary points
     G, Hvec, roots, rootcol = stationaryPts(E, x, y)
-    print(rootcol)
+    # print(roots)
+    # print(rootcol)
 
     # Calculate gradient descent path
     # assign the index of the first point
-    path = GD(roots[1], E, G, Hvec[1])
+    path = GD(roots[0], E, G, Hvec[0])
+    # path = GD(roots[1], E, G, Hvec[1])
 
     # Plot figure
-    # plotPES(np_symPES)
-    # plotPts(roots, rootcol)
-    # plotPath(path)
-    # plt.show()
+    plotPES(np_symPES)
+    plotPts(roots, rootcol)
+    plotPath(path)
+    plt.show()
--- src/plot_time_traj.sh
+++ src/plot_time_traj.sh
@@ -5,20 +5,27 @@ dir1='RP1.coord'
 dir2='RP2.coord'
 home=`pwd`
 
+# maximum limit of x-range
+xmaxList=(3.1 6.5 2.8 3.2 6.1) 
+# green area
+objiniList=(0.5 2.0 0.5 0.5 0.5)
+objfinList=(1.0 5.5 1.0 1.5 4.0)
+
 function main {
     cd $home
     rm -rf mix
     mkdir mix
 
-    makelist $dir1 mix
-    makelist $dir2 mix 
+    echo $dir1
+    makelist $dir1 mix $1
+    echo $dir2
+    makelist $dir2 mix $1 
     
+     
     cd $home/mix 
     RP1list=( $(cat RP1.coord.list) )
     RP2list=( $(cat RP2.coord.list) )
-    xlim=(3.1 6.5 2.8 3.2 6.1)
-    index=$(( $1 - 1 ))
-    plot_time_traj ${xlim[$index]}
+    plot_time_traj $1 
 
     # 
     mv RP1.eps NCH$1.RP1.eps 
@@ -30,27 +37,92 @@ function main {
 function makelist {
     # $1 = name of dir.
     # $2 = final dir.
+    # $3 = indes of selected system
     # output: $1.list 
 
     cd $home/$1
     ls | grep coord > list 
+    nTraj=$(wc -l list | awk '{print $1}')
+    sumTraj=0
     for name in `cat list`
-    do 
-        nl $name | awk '{print $2,$1}' > new.$name
+    do  
+        TSS1toTSS2 $name # output: TSS1toTSS2$name
+        nl TSS1toTSS2$name | awk '{print $2,$1}' > new.$name
+        #
+        countGreenT $3 new.$name # output: $singleTraj
+        # echo 'new'.$name $singleTraj
+        sumTraj=$(( $sumTraj + $singleTraj))
     done 
+    # TODO:
     awk '{print "new."$1}' list > $1.list 
-    rm -f list
+    avgTraj=$(echo "$sumTraj/$nTraj" | bc -l )
+    echo $nTraj $avgTraj 
+
+    rm -f list TSS1toTSS2*
 
     mv new.* $home/$2 
     mv $1.list $home/$2 
 }
 
+function countGreenT {
+    # $1 = index of system 
+    # $2 = name of traj
+    # output: 
+    # modify the value of $singleTraj
 
+    index=$(( $1 - 1 ))
+    objini=${objiniList[$index]}
+    objfin=${objfinList[$index]}
+
+    awk '{print $1}' $2 > testTraj
+    i=0
+    singleTraj=0
+    while read LINE 
+    do 
+        i=$(( $i + 1 ))
+        testXcoord=$(sed -n "$i,$i p" testTraj)
+        if (( $(echo "$testXcoord > $objini" | bc -l) \
+            && $(echo "$testXcoord < $objfin" | bc -l) ))
+        then
+            singleTraj=$(( $singleTraj + 1 )) 
+        fi 
+    done < testTraj
+    rm -f testTraj
+}
+
+function TSS1toTSS2 {
+    # $1 = name of the file
+
+    # 1. Define the position of TSS1
+      negSign=$(awk '{print $1}' $1 | grep -c '-')
+      numL=$(($negSign + 1))
+
+    # nl $1 | awk '{print $1,$2}' | grep '0.00000' \
+    #     | grep -v '-' | awk '{print $1}' > x.dat 
+    # nl $1 | awk '{print $1,$3}' | grep '0.00000' \
+    #     | grep -v '-' | awk '{print $1}' > y.dat 
+    # while read -r line
+    # do 
+
+    # done
+
+    totL=$(wc -l $1 | awk '{print $1}')
+    sed -n "$numL,$totL p" $1 > TSS1toTSS2$1 
+}
 
 function plot_time_traj {
-    # $1 = max value of x
+    # $1 = index depends on selected system
+
+    index=$(( $1 - 1 ))
+
+    xmax=${xmaxList[$index]}
+    objini=${objiniList[$index]}
+    objfin=${objfinList[$index]}
+
 gnuplot << EOF
+# postscript does't support transparent
 set term postscript eps enhanced color font 'Times-Roman,20' size 4,4
+# set term png tranparent enhanced color font 'Times-Roman,20' size 4,4
 set output "RP1.eps"
 unset key
 
@@ -58,10 +130,14 @@ RP1list="${RP1list[*]}"
 RP2list="${RP2list[*]}"
 
 set size square
-set xrange [0:$1]
-set yrange [20:200]
+set xrange [0:$xmax]
+# set yrange [20:200]
+set yrange [0:100]
 set ylabel 'fs'
-set xlabel 'VRI region ({/Symbol @{\140\140\140}\326}amu Bohr)'
+set xlabel 'TSS1 {/Symbol \256} TSS2 ({/Symbol @{\140\140\140}\326}amu Bohr)'
+set style rect fillcolor rgb "seagreen" fill transparent solid 0.5 noborder  
+set obj rect from $objini, graph 0 to $objfin, graph 1 #front
+
 plot for [file in RP1list] file u 1:2 w line lw 3 lc rgb 'red'
 
 set output "RP2.eps"
